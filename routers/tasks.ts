a833b2012bf1243e53472248729841f5518614a2
Sadykov Arsen <LoCo10291998@gmail.com>
2025-04-22 16:50:19 +0600

    Created get request for tasks.

--- routers/tasks.ts
+++ routers/tasks.ts
@@ -1,11 +1,17 @@
-import express from "express";
-import mongoose from "mongoose";
+import express, {Request} from "express";
+import mongoose, {HydratedDocument} from "mongoose";
 import User from "../models/User";
 import Task from "../models/Task";
+import {UserFields} from "../types";
+import auth from "../middleware/auth";
 
 const tasksRouter = express.Router();
 
-tasksRouter.post('/', async (req, res, next) => {
+export interface RequestWith extends Request {
+    user: HydratedDocument<UserFields>;
+}
+
+tasksRouter.post('/',auth, async (req, res, next) => {
     try {
         const token = req.get('Authorization');
 
@@ -14,7 +20,7 @@ tasksRouter.post('/', async (req, res, next) => {
             return;
         }
 
-        const user = await User.findOne({token: token });
+        const user = (req as RequestWith).user;
         if (!user) {
             res.status(401).send({ error: 'User not found' });
             return;
@@ -37,6 +43,38 @@ tasksRouter.post('/', async (req, res, next) => {
         return;
     } catch (error) {
         next(error);
+    }
+});
+
+tasksRouter.get('/', auth, async (req, res, next) => {
+    try {
+        const user = (req as RequestWith).user;
+        const user_id = req.query._id as string;
+        const filter: {user?: string} = {};
+
+        if (user_id) filter.user = user_id;
+
+        const tasks = await Task.find(filter);
+        res.send({tasks});
+    } catch (e) {
+        next(e);
+    }
+});
+
+tasksRouter.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
+
+    try {
+        const product = await Task.findById(id);
+
+        if (!product) {
+            res.status(404).send({message: 'Product not found'});
+            return;
+        }
+
+        res.send(product);
+    } catch (e) {
+        next(e);
     }
 });
 
